ad47ac57d7c76bf56ee7a2e4ef112a4f1fda5de5
Zuzanna Bujak <zuzannabujak89@gmail.com>
2024-09-24 18:37:44 +0200

    Update task-1.js

---
+++ js/task-1.js
@@ -0,0 +1,11 @@
+console.log(`Number of categories: ${categoriesList.length}`);
+
+categoriesList.forEach((category) => {
+
+  const categoryName = category.querySelector("h2").textContent;
+
+  const categoryItems = category.querySelectorAll("ul li").length;
+
+  console.log(`Category: ${categoryName}`);
+  console.log(`Elements: ${categoryItems}`);
+});
