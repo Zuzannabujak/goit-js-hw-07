60aba027b2dfa5b28ea7d0e29a59adeff9b5ee3c
Zuzanna Bujak <zuzannabujak89@gmail.com>
2024-09-24 19:03:33 +0200

    Update task-4.js

---
+++ js/task-4.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.querySelector(".login-form");
+  
+    loginForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const email = loginForm.elements.email.value.trim();
+      const password = loginForm.elements.password.value.trim();
+      if (email === "" || password === "") {
+        alert("All form fields must be filled in");
+      } else {
+        const formData = {
+          email: email,
+          password: password,
+        };
+        console.log(formData);
+        loginForm.reset();
+      }
+    });
+  });
